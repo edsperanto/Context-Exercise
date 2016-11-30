3477a6315bdc12ba0fe70ff3190b5d6b85a92f46
Edward Gao <egao@sent.com>
2016-11-29 22:58:57 -1000

    invoked add function

--- context.js
+++ context.js
@@ -1,7 +1,7 @@
 var Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
@@ -17,9 +17,9 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log( add(0, 1) );
 
-// #2  
+// #2
 console.log(  );
 
 // #3
